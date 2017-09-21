57fd4dbb014e5aa82b581945f27e95e45a5befa0
TJ Loughry <tjloughry@gmail.com>
2017-09-20 21:37:20 -0500

    TJs attempt at the deck of cards homework

--- deck-o-cards.js
+++ deck-o-cards.js
@@ -9,78 +9,52 @@ function deck_o_cards() {
 
   // Make 52 card objects and store them in the "cards" array
   // Hint: use 2 for loops
-  for (var i = 0; i < values.length; i++){
+  for (var i = 0; i < values.length; i++) {
     for (var k = 0; k < suits.length; k++) {
-      var singleCards ={Suit: suits[k], Value: values[i]};
+      var singleCards = { Suit: suits[k], Value: values[i] };
       cards.push(singleCards);
     }
-    
-  
-  }  
-  
-    shuffledCards = shuffle(cards); 
-    return shuffledCards;
+  }
+
+  shuffledCards = shuffle(cards);
+  return shuffledCards;
 
   // 2. Shuffle the cards
   // Hint: shuffle function is already defined below
-function shuffle(array) {
-    var counter = array.length, temp, index;
+  function shuffle(array) {
+    var counter = array.length,
+      temp,
+      index;
 
     // While there are elements in the array
     while (counter > 0) {
-        // Pick a random index
-        index = Math.floor(Math.random() * counter);
+      // Pick a random index
+      index = Math.floor(Math.random() * counter);
 
-        // Decrease counter by 1
-        counter--;
+      // Decrease counter by 1
+      counter--;
 
-        // And swap the last element with it
-        temp = array[counter];
-        array[counter] = array[index];
-        array[index] = temp;
+      // And swap the last element with it
+      temp = array[counter];
+      array[counter] = array[index];
+      array[index] = temp;
     }
 
     return array;
-}
+  }
 }
 
 function topCard(cards) {
   var topCard = cards[0];
- 
+
   console.log('The deck has ' + cards.length + 'cards');
-  console.log('The top card is the ' + topCard.values + ' of ' + topCard.suits );
+  console.log('The top card is the ' + topCard.values + ' of ' + topCard.suits);
 }
-  
-  topCard(deck_o_cards());
-// var topCard = cards[0]; // pulls the top card from the deck
-  
-// -  // Pull the top card from the newly shuffledCards
-// console.log(`The deck has ${cards.length} cards`); // logs the size of the deck
-// console.log(`The top card is the ${topCard.value} of ${topCard.suit}`);
-// +    // logs the value and suit of the top card
 
-
-
-
+topCard(deck_o_cards());
 
 //   // Pull the top card from the newly shuffledCards
-  
-//   function topCard (cards) {
-//     var topCard =cards[0];
-// }
 
-//   console.log(topCard)
-
-
-
-
-
-
-  // 3. Print the results:
-  // "The deck has {n} cards"
-  // "The top card is the {value} of {suit}"
-
-
-
-
-
+// 3. Print the results:
+// "The deck has {n} cards"
+// "The top card is the {value} of {suit}"
